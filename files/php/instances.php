5ba1f4bed778e231e6968af782e91d5d58033769
allo1890 <53498636+allo1890@users.noreply.github.com>
2024-09-11 21:20:08 +0200

    Add files via upload

---
+++ files/php/instances.php
@@ -0,0 +1,46 @@
+<?php
+$instance['Emauzium'] = array_merge($instance['Emauzium'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.9.4",
+        "loadder_type" => "NeoForge",
+        "loadder_version" => "latest"
+    ),
+    "verify" => true,
+    "ignored" => array(
+        'config',
+        'essential',
+        'logs',
+        'resourcepacks',
+        'saves',
+        'screenshots',
+        'shaderpacks',
+        'W-OVERFLOW',
+        'options.txt',
+        'optionsof.txt'
+    ),
+    "whitelist" => array(),
+    "whitelistActive" => false,
+    "status" => array(
+        "nameServer" => "Emauzium",
+        "ip" => "5.48.130.227",
+        "port" => 25577
+    )
+));
+
+$instance['Emauzium'] = array_merge($instance['Emauzium'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.9.4",
+        "loadder_type" => "NeoForge",
+        "loadder_version" => "latest"
+    ),
+    "verify" => false,
+    "ignored" => array(),
+    "whitelist" => array(),
+    "whitelistActive" => false,
+    "status" => array(
+        "nameServer" => "Emauzium",
+        "ip" => "5.48.130.227",
+        "port" => 25577
+    )
+));
+?>
